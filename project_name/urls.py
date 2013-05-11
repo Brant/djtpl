b71b1933f78ded61ea762949b5292b583a7c0e8d
Brant Steen <brant.steen@gmail.com>
2013-05-11 16:32:16 -0400

    forgot the urls

--- project_name/urls.py
+++ project_name/urls.py
@@ -3,7 +3,7 @@ Main URL configuraitons for {{ project_name }}
 """
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-
+from django.views.generic import TemplateView
 
 admin.autodiscover()
 
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+    url(r'^$', TemplateView.as_view(template_name="website/home.html"), name='home'),
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
