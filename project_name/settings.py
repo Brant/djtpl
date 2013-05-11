62dc9a1d9cafd7aa58388edd4a9e174f0d6021a6
Brant Steen <brant.steen@gmail.com>
2013-05-10 22:34:36 -0400

    continuing to add in my defaults... haven't touched front-end yet

--- project_name/settings.py
+++ project_name/settings.py
@@ -6,7 +6,6 @@ PROJECT_PATH = os.path.dirname(
         os.path.abspath(os.path.dirname(__file__)
     )
 )
-print PROJECT_PATH
 
 
 DEBUG = True
@@ -59,7 +58,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = PROJECT_PATH + "/assets/uploads/"
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -130,7 +129,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
