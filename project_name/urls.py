a5761d62635c469d513f29baf0bd5ef64c361afb
Brant Steen <brant.steen@gmail.com>
2013-06-11 17:26:09 -0400

    bodyclass block, non-shiny ios icons, import favicon patterns

--- project_name/urls.py
+++ project_name/urls.py
@@ -7,6 +7,7 @@ from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
 
+from noodles.urls import favicon_patterns
 
 admin.autodiscover()
 
